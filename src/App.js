bb9e7740ed02f7ec60fdcea8ef7786bc8dbcda4e
細川智晃 <136974787+cofferbreak@users.noreply.github.com>
2023-06-22 12:52:30 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [face, setFace] = useState(true);
+
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
-  useState();
+  const onClickFace = () => {
+    setFace(!face);
+  };
+  useEffect(() => {
+    if (num % 3 === 0) {
+      face || setFace(true);
+    } else {
+      face && setFace(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>Hello!</h1>
       <ColorfulMessage color="blue">how are you?</ColorfulMessage>
       <ColorfulMessage color="pink">yes</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickFace}>on/off</button>
+      <p>{num}</p>
+      {face && <p>(^_^;)</p>}
     </>
   );
 };
